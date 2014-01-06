e9000324e82bbf83cb926d517b00e5f615b4fbf6
Sirajudeen <siraajudeeen@gmail.com>
2014-01-06 17:26:40 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,26 +3,27 @@ var deviceHeading, deviceName, deviceVersion;
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-        console.log('onLoad');
-        navigator.compass.getCurrentHeading(
+        //console.log('onLoad');
+       /* navigator.compass.getCurrentHeading(
                 function(heading){
                         deviceHeading = heading;
                 },
                 function(){
                         deviceHeading = 'Not defined';
                 }
-        );
+        );*/
         deviceName = window.device.name;
         deviceVersion = window.device.version;
 }
 
 
 function doStuff(){
-        console.log('This is a console log, in the cloud!');
+        alert(deviceName);
+        /*console.log('This is a console log, in the cloud!');
         console.log('Device Heading');
         console.log(deviceHeading);
         console.log('Device Name');
         console.log(deviceName);
         console.log('Device Version');
-        console.log(deviceVersion);
+        console.log(deviceVersion);*/
 }
